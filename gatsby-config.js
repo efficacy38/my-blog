69a99a324c724c8b380967adeaeb35fd188d3e4f
efficacy38 <efficacy38@gmail.com>
2025-01-26 02:52:38 +0800

    feat: add blog post feature

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,14 +4,24 @@
 module.exports = {
   siteMetadata: {
     title: `The csjhuang Blog`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-transformer-remark", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+  plugins: [
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        "name": "pages",
+        "path": `${__dirname}/src/pages/`,
+      },
+      __key: "pages",
     },
-    __key: "pages"
-  }]
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
